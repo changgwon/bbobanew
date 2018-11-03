5fb3f61ff5fa91d7155c861fbcc724d058af6099
binnysoo <bin430@naver.com>
2018-11-03 20:18:56 +0900

    rails g controller home + view files

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def new2
+  end
+
+  def main
+  end
+
+  def fileupload
+  end
+
+  def filecreate
+  end
+
+  def filehistory
+  end
+
+  def filecurrent
+  end
+
+  def ownerpage
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'home#new2'
+  get 'home/new2'
+  get 'home/main'
+  get 'home/fileupload'
+  get 'home/filecreate'
+  get 'home/filehistory'
+  get 'home/filecurrent'
+  get 'home/ownerpage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
