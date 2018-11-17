ed2809a0bc5d27ce27dedb9cbd4a066382b77ff8
chosunme96 <31688874+chosunme96@users.noreply.github.com>
2018-11-17 14:16:03 +0900

    minor changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,36 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
+	// Requires jQuery
+
+	$(document).on('click','.js-menu_toggle.closed',function(e){
+		e.preventDefault(); $('.list_load, .list_item').stop();
+		$(this).removeClass('closed').addClass('opened');
+
+		$('.side_menu').css({ 'left':'0px' });
+
+		var count = $('.list_item').length;
+		$('.list_load').slideDown( (count*.6)*100 );
+		$('.list_item').each(function(i){
+			var thisLI = $(this);
+			timeOut = 100*i;
+			setTimeout(function(){
+				thisLI.css({
+					'opacity':'1',
+					'margin-left':'0'
+				});
+			},100*i);
+		});
+
+	});
+
+	$(document).on('click','.js-menu_toggle.opened',function(e){
+		e.preventDefault(); $('.list_load, .list_item').stop();
+		$(this).removeClass('opened').addClass('closed');
+
+		$('.side_menu').css({ 'left':'-250px' });
+
+		var count = $('.list_item').length;
+		$('.list_item').css({
+			'opacity':'0',
+			'margin-left':'-20px'
+		});
+		$('.list_load').slideUp(300);
+	});
