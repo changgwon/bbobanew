12e4cb231fda18fa310806e2aac6ac562da0284d
changgwon <rnjs9409@naver.com>
2018-11-18 22:40:04 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_17_023753) do
+ActiveRecord::Schema.define(version: 2018_11_18_133836) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_11_17_023753) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "userid"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
