97c0086ab3450e7f3f15b785730b305425356206
paekjisoo <37473753+paekjisoo@users.noreply.github.com>
2019-03-02 18:49:01 +0900

    printer 상태 수정

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get 'home/changeState1/:id'=>'home#changeState1'
   get 'home/changeState2/:id'=>'home#changeState2'
   get 'home/changeState3/:id'=>'home#changeState3'
+  get 'home/changeState4/:id'=>'home#changeState4'
   get 'payment/new' => 'payment#new'
   get 'home/filedetail/:id' => 'home#filedetail'
   get 'home/index' => 'home#index'
